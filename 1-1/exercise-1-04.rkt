08f75adef46dfcc4629f5de35c3741ec6125cb3d
Roganjoshua98 <35848559+Roganjoshua98@users.noreply.github.com>
2022-08-20 15:08:46 +0100

    exercise 1.4

---
+++ 1-1/exercise-1-04.rkt
@@ -0,0 +1,11 @@
+#lang sicp
+
+; Exercise 1-4
+; Observe that our model of evaluation allows for combinations whose operators are compound expressions. Use this observation to describe the behavior of the following procedure:
+
+(define (a-plus-abs-b a b)
+  ((if (> b 0) + -) a b))
+
+; the expression (if (> b 0) + -) is a conditional that decides whether the following expression is
+; (+ a b) or (- a b) depending on whether b is positive or negative. in other words, it's a function
+; returning an operation. interesting implications!

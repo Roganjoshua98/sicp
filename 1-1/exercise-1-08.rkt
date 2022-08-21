ae618fa65807b09a2029ba86192f022b505ad71c
Roganjoshua98 <35848559+Roganjoshua98@users.noreply.github.com>
2022-08-21 19:00:03 +0100

    reformate exercise 1.8

--- 1-1/exercise-1-08.rkt
+++ 1-1/exercise-1-08.rkt
@@ -8,17 +8,18 @@
 (define (square x)
   (* x x))
 
-(define (good-enough? guess x)
-  (= (improve guess x) guess))
-
-(define (improve guess x)
-    (/ (+ (/ x (square guess))
-          (* 2 guess))
-        3))
-
-(define (3rt-iter guess x) 
-  (if (good-enough? guess x) guess
-      (3rt-iter (improve guess x) x) ) )
-
 (define (3rt x) 
+
+    (define (good-enough? guess x)
+    (= (improve guess x) guess))
+
+    (define (improve guess x)
+        (/ (+ (/ x (square guess))
+            (* 2 guess))
+            3))
+
+    (define (3rt-iter guess x) 
+    (if (good-enough? guess x) guess
+        (3rt-iter (improve guess x) x) ) )
+
   (3rt-iter 1.1 x))

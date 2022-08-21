80a23d7d0da2fef0c4ed00a4cb4313b43853c2f5
Roganjoshua98 <35848559+Roganjoshua98@users.noreply.github.com>
2022-08-21 19:04:51 +0100

    .

--- 1-1/exercise-1-08.rkt
+++ 1-1/exercise-1-08.rkt
@@ -9,17 +9,13 @@
   (* x x))
 
 (define (3rt x) 
-
-    (define (good-enough? guess x)
-    (= (improve guess x) guess))
-
-    (define (improve guess x)
+    (define (good-enough? guess)
+        (= (improve guess x) guess))
+    (define (improve guess)
         (/ (+ (/ x (square guess))
-            (* 2 guess))
-            3))
-
-    (define (3rt-iter guess x) 
-    (if (good-enough? guess x) guess
-        (3rt-iter (improve guess x) x) ) )
-
+              (* 2 guess))
+           3))
+    (define (3rt-iter guess) 
+      (if (good-enough? guess x) guess
+          (3rt-iter (improve guess x) x)))
   (3rt-iter 1.1 x))

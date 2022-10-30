341078aaf8612164ec619b3a682d67b5bb114ecd
Roganjoshua98 <35848559+Roganjoshua98@users.noreply.github.com>
2022-10-30 12:35:23 +0000

    exercise 1-21 and also cba with 1-20

---
+++ 1-2/exercise-1-20.rkt
@@ -0,0 +1,13 @@
+#lang sicp
+
+; Exercise 1.20:
+; Thee process that a procedure generates is of course dependent on the rules used by the interpreter.
+; As an example, consider the iterative gcd procedure given above. Suppose we were to interpret this
+; procedure using normal-order evaluation, as discussed in Section 1.1.5.
+; (The normal-order-evaluation rule for if is described in Exercise 1.5.)
+; Using the substitution method (for normal order), illus- trate the process generated in evaluating
+; (gcd 206 40) and indicate the remainder operations that are actually per- formed.
+; How many remainder operations are actually performed in the normal-order evaluation of (gcd 206 40)?
+; In the applicative-order evaluation?
+
+; actually cba lol
